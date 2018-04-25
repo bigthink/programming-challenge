6df0dea1dc443fad968b47f5316f157755e79102
eal6gf <elum1234@gmail.com>
2018-04-25 17:39:01 -0400

    seed developed

--- myvideoapp/app/controllers/videos_controller.rb
+++ myvideoapp/app/controllers/videos_controller.rb
@@ -3,4 +3,9 @@ class VideosController < ApplicationController
   def index
     @videos = Video.all
   end
+
+  def find
+    @video = Video.find(params[:id])
+    # give chart thing its data
+  end
 end
--- myvideoapp/db/seeds.rb
+++ myvideoapp/db/seeds.rb
@@ -7,3 +7,51 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # need to write out a a seed file for distributed data
+require 'faker'
+require 'distribution'
+
+def seedUsage(video, user)
+  @distribution = Distribution::ChiSquare.cdf(Random.rand(), 1)
+  @usage = Usage.create!(video_id: video.id, user_id: user.id, view_time: 100 * @distribution)
+  if !@usage.save
+    throw `Could not save video_usage with errors: #{@usage.errors.full_messages}`
+  end
+end
+
+def seedVideo
+  @video = Video.create!(name: Faker::Name.name)
+  if !@video.save
+    throw `Could not save video with errors: #{@video.errors.full_messages}`
+  end
+end
+
+def seedUser
+  @user = User.create!(name: Faker::Name.name)
+  if !@user.save
+    throw `Could not save User with errors: #{@user.errors.full_messages}`
+  end
+end
+
+def clearData
+  Video.destroy_all
+  User.destroy_all
+  Usage.destroy_all
+end
+
+def seedFile
+  clearData
+
+  10.times do
+    seedVideo()
+    seedUser()
+  end
+
+  @myVideos = Video.all
+  @myUsers = User.all
+  100.times do
+    seedUsage(@myVideos.sample, @myUsers.sample)
+  end
+
+end
+
+seedFile()
